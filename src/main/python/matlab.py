b85f1312273806800a27c38237b819e0d9572381
alanktwong <alan_kt_wong@yahoo.com>
2012-08-02 12:55:51 -0400

    play with matlab-like capabilities in python

---
+++ src/main/python/matlab.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import math
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+
+# NumPy: see http://www.scipy.org/Tentative_NumPy_Tutorial#head-c5f4ceae0ab4b1313de41aba9104d0d7648e35cc
+# matplotlib: http://matplotlib.sourceforge.net/index.html
+# SciPy for Matlab users: http://www.scipy.org/NumPy_for_Matlab_Users/
+def buildMatrix():
+	matrix = np.zeros( (3,4) )
+	print "built 3x4 matrix filled with zeros"
+	return matrix
