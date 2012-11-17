828252e6ff1ee20c148f8379f2d6f0041cbccaa7
awong <alan_kt_wong@yahoo.com>
2012-11-17 13:31:00 -0500

    more hacking in haskell

--- src/main/haskell/learn/first.hs
+++ src/main/haskell/learn/first.hs
@@ -1,3 +1,8 @@
+-- my first snippet of haskell code
+-- @author awong
+-- @see http://learnyouahaskell.com/
+-- 
+
 add a b = a + b
 
 doubleMe x = x + x
@@ -9,3 +14,36 @@ doubleSmallNumber x = if x > 100
                       else doubleMe x
 
 conanO'Brien = "It's a me, Conan O'Brien"
+
+removeNonUppercase :: [Char] -> [Char]  
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
+
+addThree :: Int -> Int -> Int -> Int  
+addThree x y z = x + y + z
+
+lucky :: (Integral a) => a -> String  
+lucky 7 = "LUCKY NUMBER SEVEN!"  
+lucky x = "Sorry, you're out of luck, pal!"
+
+sayMe :: (Integral a) => a -> String  
+sayMe 1 = "One!"  
+sayMe 2 = "Two!"  
+sayMe 3 = "Three!"  
+sayMe 4 = "Four!"  
+sayMe 5 = "Five!"  
+sayMe x = "Not between 1 and 5"
+
+-- factorial defined using pattern matching & recursively
+factorial :: (Integral a) => a -> a  
+factorial 0 = 1  
+factorial n = n * factorial (n - 1)
+
+
+charName :: Char -> String  
+charName 'a' = "Alpha"  
+charName 'b' = "Bravo"  
+charName 'c' = "Charlie"
+charName x = "does not compute"
+
+addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)  
+addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
