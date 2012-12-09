c460bbd35e5f521da3792f81a55db5d0694dc450
awong <alan_kt_wong@yahoo.com>
2012-12-09 17:36:39 -0500

    Change to multi-module build

---
+++ scala_builds/sbt-hello/core/main/scala/org/awong/HelloWorld.scala
@@ -0,0 +1,5 @@
+package org.awong
+
+object HelloWorld {
+    def main(args:Array[String]) = println("This is the core package. Hi!"org)
+}
---
+++ scala_builds/sbt-hello/eventing/main/scala/org/awong/HelloAkka.scala
@@ -0,0 +1,5 @@
+package org.awong
+
+object HelloAkka {
+    def main(args:Array[String]) = println("This is the eventing package. Hi!"org)
+}
