dae327b2a8223416a516c2b4faf5c44d34507ac4
awong <alan_kt_wong@yahoo.com>
2013-06-18 16:20:17 -0400

    learn me a haskell for great good

--- src/main/haskell/learn/first.hs
+++ src/main/haskell/learn/first.hs
@@ -13,12 +13,21 @@ doubleSmallNumber x = if x > 100
                       then x
                       else doubleMe x
 
+doubleSmallNumber' x = (doubleSmallNumber x) + 1
+
 conanO'Brien = "It's a me, Conan O'Brien"
 
-removeNonUppercase :: [Char] -> [Char]  
+-- From "Types and Typeclasses" chapter 
+-- common Haskell types: Int, Integer, Float, Double, Bool, Char, String, [], ()
+-- common typeclasses: Eq, Ord, Show, Read, Enum, Bounded, Num, Integral, Floating
+
+boomBangs :: [Int] -> [String]
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+
+removeNonUppercase :: [Char] -> [Char]
 removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
 
-addThree :: Int -> Int -> Int -> Int  
+addThree :: Int -> Int -> Int -> Int
 addThree x y z = x + y + z
 
 -- From "Syntax in Functions" chapter 
