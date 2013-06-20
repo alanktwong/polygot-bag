ecc627abe7061b0607d4261d37afe1f82aa91564
awong <alan_kt_wong@yahoo.com>
2013-06-20 17:05:58 -0400

    learn me a haskell for great good

--- src/main/haskell/learn/datatypes.hs
+++ src/main/haskell/learn/datatypes.hs
@@ -2,6 +2,10 @@
 From "Making Our Own Types and Typeclasses" chapter
 @author awong
 @see http://learnyouahaskell.com/making-our-own-types-and-typeclasses#type-parameters
+
+Typeclasses are like interfaces. A typeclass defines some behavior (like comparing for equality, comparing for ordering, enumeration) and then types that can behave in that way are made instances of that typeclass. The behavior of typeclasses is achieved by defining functions or just type declarations that we then implement. So when we say that a type is an instance of a typeclass, we mean that we can use the functions that the typeclass defines with that type.
+
+Typeclasses have pretty much nothing to do with classes in languages like Java or Python. This confuses many people, so forget everything you know about classes in imperative languages right now.
 -}
 
 import qualified Data.Map as Map
@@ -35,7 +39,6 @@ vectMult :: (Num t) => Vector t -> t -> Vector t
 scalarMult :: (Num t) => Vector t -> Vector t -> t  
 (Vector i j k) `scalarMult` (Vector l m n) = i*l + j*m + k*n
 
--- Typeclasses are more like Java interfaces than Java classes
 
 data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday   
            deriving (Eq, Ord, Show, Read, Bounded, Enum)
@@ -83,7 +86,6 @@ lockers = Map.fromList
 data List a = Empty | Cons a (List a) deriving (Show, Read, Eq, Ord)
 
 data List a = Empty | Cons a (List a) deriving (Show, Read, Eq, Ord)
-
 -}
 
 {-
@@ -95,6 +97,87 @@ infixr 5  .++
 Empty .++ ys = ys  
 (x :-: xs) .++ ys = x :-: (xs .++ ys)
 -}
+
+
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
+
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+  
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+    | x == a = Node x left right
+    | x < a  = Node a (treeInsert x left) right
+    | x > a  = Node a left (treeInsert x right)
+
+
+treeElem :: (Ord a) => a -> Tree a -> Bool  
+treeElem x EmptyTree = False  
+treeElem x (Node a left right)  
+    | x == a = True  
+    | x < a  = treeElem x left  
+    | x > a  = treeElem x right
+
+{-
+Here's an example of datatype implementing a typeclass.
+`class` is for defining new typeclasses and `instance` is for
+making our types instances of typeclasses.
+
+Example of subclassing ...
+
+class (Eq a) => Num a where
+-}
+
+data TrafficLight = Red | Yellow | Green
+
+instance Eq TrafficLight where  
+    Red == Red = True  
+    Green == Green = True  
+    Yellow == Yellow = True  
+    _ == _ = False
+
+instance Show TrafficLight where  
+    show Red = "Red light"  
+    show Yellow = "Yellow light"  
+    show Green = "Green light"
+
+{-
+Mimicing truthy behavior of Javascript
+-}
+
+class YesNo a where
+    yesno :: a -> Bool
+
+
+instance YesNo Int where
+    yesno 0 = False
+    yesno _ = True
+
+
+instance YesNo [a] where
+    yesno [] = False
+    yesno _ = True
+
+instance YesNo Bool where  
+    yesno = id
+
+
+instance YesNo (Maybe a) where  
+    yesno (Just _) = True  
+    yesno Nothing = False
+
+instance YesNo (Tree a) where  
+    yesno EmptyTree = False  
+    yesno _ = True
+
+instance YesNo TrafficLight where  
+    yesno Red = False  
+    yesno _ = True
+
+yesnoIf :: (YesNo y) => y -> a -> a -> a  
+yesnoIf yesnoVal yesResult noResult = if yesno yesnoVal then yesResult else noResult
+
 
 
 
