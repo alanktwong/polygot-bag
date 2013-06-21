cdcab797ca0271be0cea8ae2a6776ccd002c187e
awong <alan_kt_wong@yahoo.com>
2013-06-21 11:18:42 -0400

    learn me a haskell for great good

---
+++ src/main/haskell/learn/helloworld.hs
@@ -0,0 +1,12 @@
+{-
+'hello, world' finally!
+
+Compiling:
+$ ghc --make helloworld  
+[1 of 1] Compiling Main             ( helloworld.hs, helloworld.o )  
+Linking helloworld ...
+
+Now executing:
+$ ./helloworld
+-} 
+main = putStrLn "hello, world"
