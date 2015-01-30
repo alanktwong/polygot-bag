2406e86a905acc16770c332971380cf71dedb6d3
Alan Wong <alan_kt_wong@yahoo.com>
2015-01-29 19:18:19 -0500

    Prepare builds for tomorrow

--- java-quickie/src/main/java/org/awong/threads/App.java
+++ java-quickie/src/main/java/org/awong/threads/App.java
@@ -5,4 +5,16 @@ public class App {
 	public static void main(String[] args) {
 		System.out.print("hi");
 	}
+	
+	private static void createThreadUsingRunnable() {
+		Runnable task = new Runnable()  {
+			@Override
+			public void run() {
+				System.out.println("hola");
+			}
+		};
+		
+		Thread th = new Thread(task);
+		th.start();
+	}
 }
