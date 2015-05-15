b0caeb9bb7ee4da89fe66913ebb5e15258269088
Alan Wong <awong@tacitknowledge.com>
2015-05-15 14:47:09 -0400

    add convenience copy scripts

---
+++ src/main/bash/cp_from_home.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cp $HOME/.bash_profile osx.bash_profile
+cp $HOME/.bashrc osx.bashrc
+cp $HOME/.profile osx.profile
---
+++ src/main/bash/cp_to_home.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cp osx.bash_profile $HOME/.bash_profile 
+cp osx.bashrc $HOME/.bashrc
+cp osx.profile $HOME/.profile 
