c9e96954f795d16a72d76f43d1b3bdceece79af0
Alan Wong <alan_kt_wong@yahoo.com>
2016-06-13 14:54:18 -0400

    Move these bash scripts to its own repo

--- src/main/bash/cp_from_home.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cp $HOME/.bash_profile osx.bash_profile
-cp $HOME/.bashrc osx.bashrc
-cp $HOME/.profile osx.profile
--- src/main/bash/cp_to_home.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cp osx.bash_profile $HOME/.bash_profile 
-cp osx.bashrc $HOME/.bashrc
-cp osx.profile $HOME/.profile 
