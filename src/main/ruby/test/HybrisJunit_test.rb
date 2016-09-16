8e49d80d13da151658be7771cfd97a8425ca6247
Alan Wong <alan_kt_wong@yahoo.com>
2016-09-16 09:30:37 -0400

    Non functional unit test

---
+++ src/main/ruby/test/HybrisJunit_test.rb
@@ -0,0 +1,21 @@
+#
+# See TK localconfig_builder_test.rb for basic idea
+#
+# To run tests: ....
+#
+require "test/unit"
+require 'rexml/document'
+
+include REXML
+class HybrisJunitTest < Test::Unit::TestCase
+  ###################################################
+  #	Helper methods
+  ###################################################
+  def load_yml_configuration
+  end
+  ###################################################
+  # Tests
+  ###################################################
+  def test_something()
+  end
+end
